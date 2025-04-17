afe91355fa126675b98d856c9eccd8350dfba0cb
Kyle Adkins <kyle.m.adkins@gmail.com>
2025-04-16 21:51:00 -0400

    Navigation

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,32 @@
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const links = [
+    {
+      href: "/",
+      label: "Home",
+    },
+    {
+      href: "/journal",
+      label: "Journal",
+    },
+  ];
+
   return (
     <main className="h-screen w-screen flex">
       <div className="h-full w-[200px] shrink-0 border-r border-black/10">
-        Mood
+        <ul className="pt-8">
+          {links.map((link) => (
+            <li key={link.href} className="px-6 py-2">
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="w-full">
         <header className="h-[60px] border-b border-black/10">
